2dbee16bb9444f93add98079e15f0a4e02c80148
sannaga3 <n.tetsuya02@gmail.com>
2021-03-15 17:38:13 +0900

    ユーザーモデルにバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :email, length: { in: 1..50 }, 
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
+  validates :email, uniqueness: true
+  validates :email, presence: true, allow_blank: true, on: :update
+  validates :email, presence: true, allow_blank: true, on: :edit_case
+  validates :password, length: { minimum: 6 }, on: :create
+  validates :password, presence: true, length: { minimum: 6 }, allow_blank: true, on: :update
+  validates :password, presence: true, length: { minimum: 6 }, allow_blank: true, on: :edit_case
+  has_many :pictures, dependent: :destroy
 end
