185a3db0cc252876bd72725eba234cf02f7ee2bc
sannaga3 <n.tetsuya02@gmail.com>
2021-03-16 13:40:17 +0900

    confirmアクションと内容確認ページ作成。ストロングパラメータ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def confirm
+    @user = params[:id] ? User.find(params[:id]) : User.new(user_params)
+    if @user.id?
+      @user.assign_attributes(user_params)
+      render :edit and return if @user.invalid?(:edit_case)
+    else
+      @user.id =  params[:id] ? params[:id] : nil
+      render :new if @user.invalid?
+    end
+  end
+
+  def show
+    
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :image, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # root :to => 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
     collection do
