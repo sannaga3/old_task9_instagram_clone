a2d26c32888bb525958bf541f8fe1666929a7b40
sannaga3 <n.tetsuya02@gmail.com>
2021-03-16 13:50:50 +0900

    createアクション及びusersヘルパー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show, :update,:destroy]
+  
   def new
     @user = User.new
   end
@@ -14,10 +16,29 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    
+  def create
+    @user = User.new(user_params)
+    if params[:back]
+      render :new
+    else
+      if @user.save
+        redirect_to user_path(@user.id), notice: "ユーザー登録完了しました！"
+      else
+        render :new
+      end
+    end
   end
 
+  def show
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   def user_params
     params.require(:user).permit(:name, :email, :image, :password, :password_confirmation)
   end
