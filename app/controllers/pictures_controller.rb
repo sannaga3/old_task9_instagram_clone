d0803d43f5b601b56b5aa736bec8b2122d594d7d
sannaga3 <n.tetsuya02@gmail.com>
2021-03-18 13:54:06 +0900

    confirmアクション及びストロングパラメータ設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,7 +4,22 @@ class PicturesController < ApplicationController
   end
 
   def new
-    @picture = Picture.new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def confirm
+    @picture = Picture.new(picture_params)
+    @picture.id = params[:id]
+    @picture.user_id = current_user.id
+    if @picture.id?
+      render :edit and return if @picture.invalid?
+    else
+      render :new if @picture.invalid?
+    end
   end
 
   def create
@@ -20,5 +35,10 @@ class PicturesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:content, :id, :post_image, :post_image_cache, :image, :image_cache)
   end
 end
