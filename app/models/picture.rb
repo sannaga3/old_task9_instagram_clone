b45ab5ab33aed63d2b84e41a340eabd57df52406
sannaga3 <n.tetsuya02@gmail.com>
2021-03-19 14:09:31 +0900

    お気に入りモデルを介し、ユーザーモデルとピクチャーモデルを関連づけ

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   validates :content, presence: true, length: {minimum:1, maximum:50}
   mount_uploader :post_image, ImageUploader
 end
