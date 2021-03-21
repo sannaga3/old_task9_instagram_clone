52878373c45f4e6b196a1dcd16c356be02e2c966
sannaga3 <n.tetsuya02@gmail.com>
2021-03-21 18:21:56 +0900

    シードデータ不具合の為再作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require 'securerandom'
 
 50.times do |n|
   name = Faker::Creature::Cat.name
@@ -12,7 +13,15 @@ require 'faker'
                )
 end
 
-
+50.times do |n|
+content = Faker::Games::Pokemon.move
+post_image = nil
+user_id = Random.rand( 1..50 )
+Picture.create!(content: content,
+               post_image: post_image,
+               user_id: user_id,
+               )
+end
 
 
 
