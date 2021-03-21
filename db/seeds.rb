64dde7a7a3115eeb58e795fcad017afd1f070353
sannaga3 <n.tetsuya02@gmail.com>
2021-03-21 17:45:57 +0900

    シードデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
 50.times do |n|
-  name = Faker::JapaneseMedia::Doraemon.location #=> "Tokyo"
+  name = Faker::Creature::Cat.name
   email = Faker::Internet.email
   image = nil
   password = "password"
@@ -12,15 +12,7 @@ require 'faker'
                )
 end
 
-50.times do |n|
-content = Faker::Games::Pokemon.move
-post_image = nil
-user_id = SecureRandom.random_number(50)
-Picture.create!(content: content,
-               post_image: post_image,
-               user_id: user_id,
-               )
-end
+
 
 
 
