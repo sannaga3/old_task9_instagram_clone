ed84aad4a6f23a6760f3911094bfd1ea1f281781
sannaga3 <n.tetsuya02@gmail.com>
2021-03-24 12:00:35 +0900

    SendGrid機能停止中のため、ピクチャーコントローラーのcreateアクションにてメール送信のコードをコメントアウト

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,6 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: "paguken6@yahoo.co.jp", subject: "投稿完了メール"
+    mail to: @picture.user.email, subject: "投稿完了メール"
   end
 end
